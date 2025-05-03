68875523518592a1f2c24747a36057953b6e33f6
MrTizu <204244194+bw3h@users.noreply.github.com>
2025-05-03 19:10:38 +0300

    Create AppCopyright.vue

---
+++ components/AppCopyright.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <v-text font="sans" size="1rem" >
+      <a href="/terms/"
+              >
+    <v-text size="1rem" >Terms</v-text></a
+            >
+      © {{ currentYear }} web3heaven.
+    </v-text>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AppCopyright',
+  computed: {
+    currentYear() {
+      return new Date().getFullYear()
+    },
+  },
+}
+</script>
+
+<style lang="css" scoped>
+div {
+  padding: 1.5rem;
+  text-align: center;
+  background-color: #00000085;
+}
+</style>
