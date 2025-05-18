3af0eb4ae39806935f65d76b44ed413a2d7eab46
MrTizu <204244194+bw3h@users.noreply.github.com>
2025-05-18 07:05:18 +0300

    Create HeroSection.vue

---
+++ components/home/HeroSection.vue
@@ -0,0 +1,104 @@
+<template>
+  <section class="hero-section">
+    <v-container>
+      <v-stack
+        justify="space-between"
+        md-direction="column-reverse"
+        md-align="center"
+        class="position-relative"
+      >
+        <v-stack direction="column" class="hero-content-container">
+          <v-heading :level="1" class="hero-title">
+            <div>Payments Freedom Powered by web3heaven.</div>            
+          </v-heading>
+          <v-stack direction="column">
+            <app-section-descriptor
+              description="web3heaven will lead you into the new digital era. 
+              Step forward with confidence as we revolutionize the way we transact, empowering individuals and businesses alike with the security and trust of the blockchain technology."
+              class="hero-description"
+            />
+            <v-stack align="center" gap="1.5rem" sm-gap="1rem" wrap>
+              <router-link to="/about/">
+                <v-button label="DISCOVER" label-transform="uppercase" />
+              </router-link>
+              <router-link to="/careers/">
+                <v-button
+                  variant="outline"
+                  label="JOIN US"
+                  label-transform="uppercase"
+                  class="secondary-button"
+                />
+              </router-link>
+            </v-stack>
+          </v-stack>
+        </v-stack>
+        <v-image
+              class="hero-image"
+              path="images/blockchain2.png"
+            />     
+      </v-stack>
+    </v-container>
+  </section>  
+</template>
+<script>
+const web3heaven_URL = process.env.web3heaven_BOOK_DEMO_URL
+const web3heaven_DASHBOARD_URL = process.env.web3heaven_DASHBOARD_URL
+
+export default {
+  name: 'HeroSection',
+
+
+  data() {
+    return {
+      isIntroPlaying: false,
+      web3heaven_URL,
+      web3heaven_DASHBOARD_URL,
+
+     
+    }
+  },
+
+}
+</script>
+
+<style lang="css" scoped>
+@import url('../lib/media-query-helper.css');
+
+.hero-section {
+  padding: 0 0 6rem;
+
+
+  @media (--viewport-small) {
+    padding: 10px 0;
+    margin-top: 0 !important;
+  }
+}
+
+.hero-image {
+  max-width: 36rem;
+  width: 100%;
+
+
+
+  @media (--viewport-medium) {
+    margin-right: 0;
+  }
+
+  @media (--viewport-small) {
+    margin-top: 2rem;
+  }
+}
+
+.hero-description {
+  margin-block: 2rem;
+}
+
+.hero-content-container {
+  max-width: 38rem;
+}
+
+.hero-title {
+  margin-block: 0;
+  max-width: 20ch;
+}
+</style>
