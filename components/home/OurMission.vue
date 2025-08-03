fc21e25e84b28302f298c213dccf2e79c1e12198
MrTizu <204244194+bw3h@users.noreply.github.com>
2025-08-03 19:29:00 +0300

    Update OurMission.vue

--- components/home/OurMission.vue
+++ components/home/OurMission.vue
@@ -3,34 +3,34 @@
     <v-container>
       <app-section-descriptor
         title="Our mission"
-        heading="web3heaven Vision"
+        heading="Luminet Vision"
         style="max-width: 60rem"
       />
 
       <app-section-descriptor
-            description="With web3heaven, users can expect lightning-fast transaction speeds, eliminating the frustrating delays that have long been associated with traditional payment systems. Our platform ensures that transactions are processed swiftly, enabling seamless and timely transfers of value."
+            description="With Luminet, users can expect lightning-fast transaction speeds, eliminating the frustrating delays that have long been associated with traditional payment systems. Our platform ensures that transactions are processed swiftly, enabling seamless and timely transfers of value."
 style="text-align: left;" />
 <div class="spc"></div>
 
       <app-section-descriptor
-            description="Our mission at web3heaven is to revolutionize the traditional payment system with our groundbreaking platform. For far too long, inefficiency, insecurity, and exorbitant costs have plagued conventional payment methods, limiting access and hindering financial progress. We are determined to disrupt this status quo by harnessing cutting-edge blockchain technologies and democratizing their benefits for all. By leveraging the power of decentralization, we empower users to unlock a transformative payment experience that surpasses the limitations of the past."
+            description="Our mission at Luminet is to revolutionize the traditional payment system with our groundbreaking platform. For far too long, inefficiency, insecurity, and exorbitant costs have plagued conventional payment methods, limiting access and hindering financial progress. We are determined to disrupt this status quo by harnessing cutting-edge blockchain technologies and democratizing their benefits for all. By leveraging the power of decentralization, we empower users to unlock a transformative payment experience that surpasses the limitations of the past."
 style="text-align: left;" />
 <div class="spc"></div>
 
 
 
 <app-section-descriptor
-            description="Security is also at the core of our mission. Traditional payment methods have often fallen victim to security breaches and data theft, exposing individuals and businesses to significant risks. web3heaven tackles this challenge head-on by implementing robust security measures, leveraging the immutability and transparency of blockchain technology. Our users can have peace of mind, knowing that their transactions are protected by state-of-the-art cryptographic protocols and secure decentralized networks."
+            description="Security is also at the core of our mission. Traditional payment methods have often fallen victim to security breaches and data theft, exposing individuals and businesses to significant risks. Luminet tackles this challenge head-on by implementing robust security measures, leveraging the immutability and transparency of blockchain technology. Our users can have peace of mind, knowing that their transactions are protected by state-of-the-art cryptographic protocols and secure decentralized networks."
 style="text-align: left;" />
 <div class="spc"></div>
 
 <app-section-descriptor
-            description="Privacy is another aspect we prioritize. We understand the importance of personal and financial privacy in an increasingly interconnected world. web3heaven empowers individuals and businesses to maintain their privacy by minimizing the collection and exposure of sensitive data. Through our platform, users can conduct transactions with confidence, knowing that their personal information is safeguarded."
+            description="Privacy is another aspect we prioritize. We understand the importance of personal and financial privacy in an increasingly interconnected world. Luminet empowers individuals and businesses to maintain their privacy by minimizing the collection and exposure of sensitive data. Through our platform, users can conduct transactions with confidence, knowing that their personal information is safeguarded."
 style="text-align: left;" />
 <div class="spc"></div>
 
 <app-section-descriptor
-            description="Cost savings are a significant advantage of the web3heaven platform. Conventional payment systems often come with exorbitant fees and complex intermediaries, adding unnecessary financial burdens for users. We believe in making payments accessible and affordable for everyone. web3heaven significantly reduces transaction costs, allowing users to retain more of their hard-earned money and stimulating economic growth."
+            description="Cost savings are a significant advantage of the Luminet platform. Conventional payment systems often come with exorbitant fees and complex intermediaries, adding unnecessary financial burdens for users. We believe in making payments accessible and affordable for everyone. Luminet significantly reduces transaction costs, allowing users to retain more of their hard-earned money and stimulating economic growth."
 style="text-align: left;" />
 <div class="spc"></div>
 
