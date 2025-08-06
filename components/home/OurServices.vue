091fc887ab566e6d4e22bf0adfb90d49b70ff88e
MrTizu <204244194+bw3h@users.noreply.github.com>
2025-08-06 21:02:07 +0300

    Update OurServices.vue

--- components/home/OurServices.vue
+++ components/home/OurServices.vue
@@ -2,9 +2,9 @@
   <section>
     <v-container>
       <app-section-descriptor
-        title="What is web3heaven?"
-        heading="Introducing web3heaven"
-        description="The groundbreaking platform poised to revolutionize the traditional payment system. Inefficiency, insecurity, and exorbitant costs have long plagued conventional payment methods. web3heaven seeks to disrupt this status quo by harnessing cutting-edge blockchain technologies, democratizing their benefits for all. With web3heaven, users can unlock the transformative power of decentralization, experiencing lightning-fast transaction speeds, enhanced security, heightened privacy, and significant cost savings. Join us on this pioneering journey as we reshape the future of payments, making it accessible, secure, and efficient for everyone."
+        title="What is Luminet?"
+        heading="Introducing Luminet"
+        description="The groundbreaking platform poised to revolutionize the traditional payment system. Inefficiency, insecurity, and exorbitant costs have long plagued conventional payment methods. Luminet seeks to disrupt this status quo by harnessing cutting-edge blockchain technologies, democratizing their benefits for all. With Luminet, users can unlock the transformative power of decentralization, experiencing lightning-fast transaction speeds, enhanced security, heightened privacy, and significant cost savings. Join us on this pioneering journey as we reshape the future of payments, making it accessible, secure, and efficient for everyone."
         style="max-width: 64rem"
       />
       <div class="overview-features">
@@ -18,7 +18,7 @@
           <div class="icon-container">
             <v-image class="serv-image" 
             path="images/speed.png" 
-            alt="web3heaven Speed" />
+            alt="Luminet Speed" />
           </div>
           <div class="overview-feature">
             <v-heading :level="2" class="overview-feature-title">
@@ -32,10 +32,10 @@
                 line-height="1.5"
                 class="feature-description-text"
               >       
-              web3heaven stands out as a fast blockchain network designed for everyday use. 
-              The web3heaven speed enables fast transaction with confirmation time of only 5 seconds, making it ideal for swift and efficient daily transactions. 
-              Whether you're making payments, conducting business, or engaging in other activities, web3heaven's rapid confirmation times ensure a seamless user experience. 
-              With its focus on speed and usability, web3heaven empowers users with a blockchain network that can keep pace with the demands of modern-day transactions. 
+              Luminet stands out as a fast blockchain network designed for everyday use. 
+              The Luminet speed enables fast transaction with confirmation time of only 5 seconds, making it ideal for swift and efficient daily transactions. 
+              Whether you're making payments, conducting business, or engaging in other activities, Luminet's rapid confirmation times ensure a seamless user experience. 
+              With its focus on speed and usability, Luminet empowers users with a blockchain network that can keep pace with the demands of modern-day transactions. 
 
               </v-text>
             </div>
@@ -62,11 +62,11 @@
                 color="secondary"
                 line-height="1.5"
                 class="feature-description-text"
-              >        With web3heaven, say "Good-bye!" to network congestion and lengthy confirmation times. 
-              web3heaven's custom BFT consensus mechanism achieve transaction finality within the same block. 
+              >        With Luminet, say "Good-bye!" to network congestion and lengthy confirmation times. 
+              Luminet's custom BFT consensus mechanism achieve transaction finality within the same block. 
               By optimizing its blockchain protocol and transaction validation process,
-              web3heaven minimizes delays and eliminates the need for extended waiting periods.
-              Say goodbye to frustrating bottlenecks and experience the convenience of fast transaction confirmations on the web3heaven platform.
+              Luminet minimizes delays and eliminates the need for extended waiting periods.
+              Say goodbye to frustrating bottlenecks and experience the convenience of fast transaction confirmations on the Luminet platform.
 
                     </v-text>
             </div>
@@ -74,7 +74,7 @@
           <div class="icon-container">
             <v-image class="serv-image"
               path="images/scalability.png"
-              alt="web3heaven Security"
+              alt="Luminet Security"
             />
           </div>
         </v-stack>
@@ -88,7 +88,7 @@
           <div class="icon-container">
             <v-image class="serv-image1"
               path="images/security.png"
-              alt="web3heaven Scalability"
+              alt="Luminet Scalability"
             />
           </div>
           <div class="overview-feature">
@@ -132,16 +132,16 @@
                 line-height="1.5"
                 class="feature-description-text"
               >
-              The web3heaven blockchain utilizes Proof-of-Stake (PoS), an energy-efficient and eco-friendly approach. 
+              The Luminet blockchain utilizes Proof-of-Stake (PoS), an energy-efficient and eco-friendly approach. 
               Unlike energy-consuming Proof-of-Work (PoW), PoS relies on stakeholder participation, eliminating resource-intensive mining. 
-              web3heaven's innovative nPoS consensus ensures efficient block verification while prioritizing sustainability.
+              Luminet's innovative nPoS consensus ensures efficient block verification while prioritizing sustainability.
               </v-text>
             </div>
           </div>
           <div class="icon-container">
             <v-image class="serv-image"
               path="images/eco5.png"
-              alt="web3heaven Eco"
+              alt="Luminet Eco"
             />
           </div>
         </v-stack>
